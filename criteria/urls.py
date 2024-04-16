c44c0dcb4a9ac2848b75b46a148d83a1a016d9fc
Yulia <yulialim0220@gmail.com>
2024-04-16 22:15:46 +0700

    rev

--- criteria/urls.py
+++ criteria/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("",views.index_criteria,name="criteria"),
     path("submit",views.submit_criteria_form,name="submit_criteria_form"),
     path("submit/edit/<int:criteria_id>",views.submit_criteria_form,name="submit_criteria_form"),
+    path("list_alt",views.get_all_alternative,name="list_alt"),
     path("list",views.get_all_criteria,name="list"),
     path("edit/<int:criteria_id>",views.get_criteria_by_id,name="get_criteria_by_id"),    
     path("delete/<int:criteria_id>",views.delete_criteria_by_id,name="delete_criteria_by_id"),
